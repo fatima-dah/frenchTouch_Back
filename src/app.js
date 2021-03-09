f03a87537517e62d94f4c9e007674eaf66bc512f
fatima-dah <dahnini.fatima@hotmail.fr>
2021-03-09 10:11:05 +0100

    finish change route

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const express = require('express');
-var cors = require('cors')
 const app = express();
+
+var cors = require('cors');
+const router = require('./routes/index.routes');
 
 app.use(express.json());
 app.use(cors());
 app.use('/api', router);
 
+
+
 app.use(express.urlencoded({extended: true}));
 
-const router = require('./routes/index.routes');
 
 
 module.exports = app;
