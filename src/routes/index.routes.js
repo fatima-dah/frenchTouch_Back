5f716cb3e0c0ebae2a597a6f4233ad5d93a86c48
Manuel <robinard.manuel@gmail.com>
2021-03-10 09:54:05 +0100

    done route reserve for visitors

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,39 +1,36 @@
-const router = require('express').Router();
-const adminRouter = require('./admin.routes.js');
-const userRouter = require('./user.routes.js');
-const bookRouter = require('./book.routes.js');
-const buyRouter = require('./buy.routes.js');
-const categoryRouter = require('./category.routes.js');
-const homeRouter = require('./home.routes.js');
-const noticeRouter = require('./notice.routes.js');
-const paletteRouter = require('./palette.routes.js');
-const productRouter = require('./product.routes.js');
-const reserveRouter = require('./reserve.routes.js');
-const service_presentationRouter = require('./service_presentation.routes.js');
-const serviceRouter = require('./service.routes.js');
-const sub_categoryRouter = require('./sub_category.routes.js');
-const giftRouter = require('./gift.routes.js');
-const imageCarouselGiftRouter = require('./imageCarouselGift.routes.js');
+const router = require("express").Router();
+const adminRouter = require("./admin.routes.js");
+const userRouter = require("./user.routes.js");
+const bookRouter = require("./book.routes.js");
+const buyRouter = require("./buy.routes.js");
+const categoryRouter = require("./category.routes.js");
+const homeRouter = require("./home.routes.js");
+const noticeRouter = require("./notice.routes.js");
+const paletteRouter = require("./palette.routes.js");
+const productRouter = require("./product.routes.js");
+const reserveRouter = require("./reserve.routes.js");
+const service_presentationRouter = require("./service_presentation.routes.js");
+const serviceRouter = require("./service.routes.js");
+const sub_categoryRouter = require("./sub_category.routes.js");
+const giftRouter = require("./gift.routes.js");
+const imageCarouselGiftRouter = require("./imageCarouselGift.routes.js");
+const reserve_visitorRouter = require("./reserve_visitor.routes.js");
 
-
-
-
-router.use('/admins', adminRouter);
-router.use('/users', userRouter);
-router.use('/books', bookRouter);
-router.use('/buys', buyRouter);
-router.use('/categorys', categoryRouter);
-router.use('/homes', homeRouter);
-router.use('/notices', noticeRouter);
-router.use('/palettes', paletteRouter);
-router.use('/products', productRouter);
-router.use('/reserves', reserveRouter);
-router.use('/services_presentation', service_presentationRouter);
-router.use('/services', serviceRouter);
-router.use('/sub_categorys', sub_categoryRouter);
-router.use('/gifts', giftRouter);
-router.use('/imagesCarouselGifts', imageCarouselGiftRouter);
-
-
+router.use("/admins", adminRouter);
+router.use("/users", userRouter);
+router.use("/books", bookRouter);
+router.use("/buys", buyRouter);
+router.use("/categorys", categoryRouter);
+router.use("/homes", homeRouter);
+router.use("/notices", noticeRouter);
+router.use("/palettes", paletteRouter);
+router.use("/products", productRouter);
+router.use("/reserves", reserveRouter);
+router.use("/services_presentation", service_presentationRouter);
+router.use("/services", serviceRouter);
+router.use("/sub_categorys", sub_categoryRouter);
+router.use("/gifts", giftRouter);
+router.use("/imagesCarouselGifts", imageCarouselGiftRouter);
+router.use("/reserve_visitor", reserve_visitorRouter);
 
 module.exports = router;
